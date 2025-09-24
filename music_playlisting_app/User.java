a2fab6a12d995e486ca2499fa341cab2d3f0e0e5
User <user@debian-BULLSEYE-live-builder-AMD64>
2025-09-23 21:39:13 -0400

    Created the user superclass and the admin, listener, artist subclasses. Created the authenticate() static method in main class. The main method tests the subclasses by creating objects with them and tests them with methods and print statements.

---
+++ music_playlisting_app/User.java
@@ -0,0 +1,42 @@
+
+/**
+ * This is the class which defines the user accounts in our application. 
+ *
+ * @authors Mateusz Gembarzewski, Michael Scandiffio, Hitiksh Doshi
+ * @project Music Playlisting App
+ * @group   HMM 
+ * @version 1
+ * 
+ * 
+ */
+public abstract class User
+{
+    // instance variables - replace the example below with your own
+    private String email;
+    private String username;
+    private String password; 
+    
+    /**
+     * Constructor for objects of class user
+     */
+    public User(String email, String username, String password)
+    {
+        // initialise instance variables
+        this.email = email;
+        this.username = username;
+        this.password = password; 
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password; 
+    }
+    
+    public String getEmail() {
+        return email; 
+    }
+
+}
