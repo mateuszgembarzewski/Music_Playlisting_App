cd9a89a8597544c231a842c50d89f05d2a87afc5
User <user@debian-BULLSEYE-live-builder-AMD64>
2025-09-24 15:30:47 -0400

    Changed the Main method to accept terminal input from the user to create an account.

--- music_playlisting_app/Main.java
+++ music_playlisting_app/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class Main { // Replace 'Main' with your chosen class name
      public static void main(String[] args) {
          
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Welcome to the HMM Music Playlisting Application!");
+        
+        System.out.println("Enter 1 to Create An Account or Press 2 to Login:");
+        
+        String input = scanner.nextLine();
+        int value = Integer.parseInt(input); 
+        System.out.println("You selected option: " + input);
+        
+        if (value == 1) {
+            System.out.println("To create an account, please enter your desired username: ");
+            String enteredUsername = scanner.nextLine();
+            System.out.println("You selected the username: " + enteredUsername);
+            System.out.println("Now please enter your desired password: ");
+            String enteredPassword = scanner.nextLine();
+        } else if (value == 2) {
+            
+        } else {
+            System.out.println("Neither value 1 or 2 was entered... skipping this step.");
+        }
+        
         Song song1 = new Song("SongName", "ArtistCreator", 300);
         
-        Song song2 = new Song("IllegalSong", "ArtistCreator", 601);
+        //Song song2 = new Song("IllegalSong", "ArtistCreator", 601);
         
         System.out.println("Song1's duration in seconds is: " + song1.getDurationInSecs());
         
